33b2c0a75002bb0892a5da2a07ad7f613331652f
wontwopunch <jiwontwopunch@gmail.com>
2024-12-11 21:48:42 +0900

    배포관련 코드수정

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use((req, res) => {
   
 // 서버 시작
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || '0.0.0.0';
 
 app.listen(PORT, HOST, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
