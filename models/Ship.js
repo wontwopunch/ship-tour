b50d39f11e4ba50caeb5967c65c7f90225140464
wontwopunch <jiwontwopunch@gmail.com>
2024-12-16 09:48:29 +0900

    요구사항 수정

--- models/Ship.js
+++ models/Ship.js
@@ -1,3 +1,7 @@
+const mongoose = require('mongoose');
+
 const ShipSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
 });
+
+module.exports = mongoose.model('Ship', ShipSchema);
